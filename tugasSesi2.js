95425832705d376e67c71da12e50f9ae169406ae
MNaufalRizky <123934543+MNaufalRizky@users.noreply.github.com>
2023-10-03 17:42:03 +0700

    Add files via upload

---
+++ tugasSesi2.js
@@ -0,0 +1,17 @@
+let firstname = "Mochamad Naufal"
+let lastname = " Rizky"
+let age = 19
+
+let ismarried = "Belum" == "Belum"
+let template = `Firstname : ${firstname} 
+Lastname ${lastname} 
+Age : ${age} 
+Belum Menikah : ${ismarried}`
+
+console.log(template)
+
+// Pembuktian type data
+console.log(typeof firstname)
+console.log(typeof lastname)
+console.log(typeof age)
+console.log(typeof ismarried)
